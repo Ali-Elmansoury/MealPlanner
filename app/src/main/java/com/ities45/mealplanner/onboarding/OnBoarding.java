d276980e9a9d7862e9fbfed9ef3d2257401fcb15
Ali-Elmansoury <ali.elmansoury21@gmail.com>
2025-04-25 16:50:34 +0300

    Added dependencies for lottie, glide, retrofit, room, and firebase. App icon is changed. Splash screen is done. Onboarding screen in progress

---
+++ app/src/main/java/com/ities45/mealplanner/onboarding/OnBoarding.java
@@ -0,0 +1,16 @@
+package com.ities45.mealplanner.onboarding;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ities45.mealplanner.R;
+
+public class OnBoarding extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding);
+    }
+}
---
+++ app/src/main/java/com/ities45/mealplanner/splash/Splash.java
@@ -0,0 +1,41 @@
+package com.ities45.mealplanner.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dotlottie.dlplayer.Mode;
+import com.ities45.mealplanner.R;
+import com.ities45.mealplanner.onboarding.OnBoarding;
+import com.lottiefiles.dotlottie.core.model.Config;
+import com.lottiefiles.dotlottie.core.util.DotLottieSource;
+import com.lottiefiles.dotlottie.core.widget.DotLottieAnimation;
+
+public class Splash extends AppCompatActivity {
+
+    private DotLottieAnimation splashAnim;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        splashAnim = findViewById(R.id.lottie_view);
+        Config config = new Config.Builder()
+                .autoplay(true)
+                .speed(1f)
+                .loop(false)
+                .source(new DotLottieSource.Asset("lottie_animated_splash.json")) // Asset from the asset folder .json or .lottie
+                .playMode(Mode.FORWARD)
+                .build();
+        splashAnim.load(config);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, OnBoarding.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
+    }
+}
