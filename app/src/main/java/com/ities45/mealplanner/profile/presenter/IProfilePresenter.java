77c561fe444200f53867e6d2740fc07fbcede7e5
Ali-Elmansoury <ali.elmansoury21@gmail.com>
2025-05-06 10:48:28 +0300

    Added ProfileFragment

---
+++ app/src/main/java/com/ities45/mealplanner/profile/presenter/IProfilePresenter.java
@@ -0,0 +1,4 @@
+package com.ities45.mealplanner.profile.presenter;
+
+public interface IProfilePresenter {
+}
---
+++ app/src/main/java/com/ities45/mealplanner/profile/view/IProfileFragmentView.java
@@ -0,0 +1,4 @@
+package com.ities45.mealplanner.profile.view;
+
+public interface IProfileFragmentView {
+}
