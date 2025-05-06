745b7ee03a2da0554f44ba68a1f655f0d730a8a7
Ali-Elmansoury <ali.elmansoury21@gmail.com>
2025-05-07 00:21:55 +0300

    changed button size, and fixed name and date text overlapping

--- app/src/main/java/com/ities45/mealplanner/mainactivity/view/ISearchCommunicator.java
+++ app/src/main/java/com/ities45/mealplanner/mainactivity/view/ISearchCommunicator.java
@@ -1,11 +1,6 @@
 package com.ities45.mealplanner.mainactivity.view;
 
-import com.ities45.mealplanner.model.pojo.Area;
-import com.ities45.mealplanner.model.pojo.Category;
-import com.ities45.mealplanner.model.pojo.Ingredient;
 
 public interface ISearchCommunicator {
-    void sendCategoryToSearchMealFragment(Category category);
-    void sendAreaToSearchMealFragment(Area area);
-    void sendIngredientToSearchMealFragment(Ingredient ingredient);
+   void onSearchItemClickedSendToSearchMeal(String itemName, String itemType);
 }
